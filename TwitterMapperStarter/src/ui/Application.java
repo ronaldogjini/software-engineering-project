a4386d0b589ca68872204eb16984a897a86c76fa
ronaldogjini <ronaldogjini@users.noreply.github.com>
2021-07-28 11:42:57 +0200

    Switched initialization from PlaybackTwitter to LiveTwitterSource

--- TwitterMapperStarter/src/ui/Application.java
+++ TwitterMapperStarter/src/ui/Application.java
@@ -7,6 +7,7 @@ import org.openstreetmap.gui.jmapviewer.interfaces.ICoordinate;
 import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
 import org.openstreetmap.gui.jmapviewer.tilesources.BingAerialTileSource;
 import query.Query;
+import twitter.LiveTwitterSource;
 import twitter.PlaybackTwitterSource;
 import twitter.TwitterSource;
 import util.SphericalGeometry;
@@ -34,14 +35,12 @@ public class Application extends JFrame {
     private TwitterSource twitterSource;
 
     private void initialize() {
-        // To use the live twitter stream, use the following line
-        // twitterSource = new LiveTwitterSource();
-
         // To use the recorded twitter stream, use the following line
         // The number passed to the constructor is a speedup value:
         //  1.0 - play back at the recorded speed
         //  2.0 - play back twice as fast
-        twitterSource = new PlaybackTwitterSource(60.0);
+
+        twitterSource = new LiveTwitterSource();
 
         queries = new ArrayList<>();
     }
